35c18fc4e087a9beaf54fe8d14bedd3334e9e4be
Miguel Ángel Valentín Naranjo <mavnaranjo@gmail.com>
2024-11-20 11:51:16 +0100

    fix: only send file path for profile image

--- src/people/routes.ts
+++ src/people/routes.ts
@@ -5,11 +5,7 @@ import me from "./me";
 const router = Router();
 
 router.get("/me", (req: Request, res: Response) => {
-  let person = { ...me };
-
-  person.image = req.app.locals.url + me.image;
-
-  res.json(person);
+  res.json(me);
 });
 
 export default router;
